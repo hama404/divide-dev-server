e1bcca7c5b883827c6908db5bc6d9a4cb42b74a9
hamasaki <mfhama9910@icloud.com>
2021-06-03 12:04:22 +0900

    add confirm url

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::SessionsController < ApplicationController
+  def whoami
+    user = {
+      admin: 'anpanman',
+      password:'hogehoge',
+      message: 'i am hero'
+    }
+    render json: user
+  end
+end
