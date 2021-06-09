ac4c982d883ece4ac6d63d3800ebb7a2121d5f02
hamasaki <mfhama9910@icloud.com>
2021-06-09 09:13:53 +0900

    implement devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'api/v1/sessions#whoami'
   get '/whoami', to: 'api/v1/sessions#whoami'
-
+  
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+   }
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
